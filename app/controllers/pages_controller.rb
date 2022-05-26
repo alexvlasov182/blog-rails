18d45bb1cca78ca67f45650d1ae06c0cb3e53bf2
Alex Vlasov <drumlife182@gmail.com>
2022-05-26 09:07:09 +0200

    Implemented twitter call on tech new page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,9 @@ class PagesController < ApplicationController
   def about; end
 
   def contact; end
+
+  def tech_news
+    @tweets = ['RubyGems.org is the Ruby community’s gem hosting service. Instantly publish your gems and then install them. Use the API to find out more about available gems. Become a contributor and improve the site yourself.', 'RubyGems.org is made possible through a partnership with the greater Ruby community. Fastly provides bandwidth and CDN support, Ruby Central covers infrastructure costs, and Ruby Together funds ongoing development and ops work. Learn more about our sponsors and how they work together.', 'We need your help to fund the developer time that keeps RubyGems.org running smoothly for everyone. Join Ruby Together today.', 'RubyGems.org is made possible through a partnership with the greater Ruby community. Fastly provides bandwidth and CDN support, Ruby Central covers infrastructure costs, and Ruby Together funds ongoing development and ops work. Learn more about our sponsors and how they work together.']
+    #@tweets = SocialTool.twitter_search
+  end
 end
