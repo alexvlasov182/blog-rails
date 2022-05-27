069e0d14eba762792211785f3dd81156597ea795
Alex Vlasov <drumlife182@gmail.com>
2022-05-27 14:08:18 +0200

    Implemented tweetkit in application and api Twitter version 2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,6 @@ class PagesController < ApplicationController
   def contact; end
 
   def tech_news
-    @tweets = ['RubyGems.org is the Ruby community’s gem hosting service. Instantly publish your gems and then install them. Use the API to find out more about available gems. Become a contributor and improve the site yourself.', 'RubyGems.org is the Ruby community’s gem hosting service. Instantly publish your gems and then install them. Use the API to find out more about available gems. Become a contributor and improve the site yourself.', 'RubyGems.org is the Ruby community’s gem hosting service. Instantly publish your gems and then install them. Use the API to find out more about available gems. Become a contributor and improve the site yourself.', 'RubyGems.org is the Ruby community’s gem hosting service. Instantly publish your gems and then install them. Use the API to find out more about available gems. Become a contributor and improve the site yourself.', 'RubyGems.org is the Ruby community’s gem hosting service. Instantly publish your gems and then install them. Use the API to find out more about available gems. Become a contributor and improve the site yourself.', 'RubyGems.org is the Ruby community’s gem hosting service. Instantly publish your gems and then install them. Use the API to find out more about available gems. Become a contributor and improve the site yourself.',]
-    #@tweets = SocialTool.twitter_search
+    @tweets = SocialTool.twitter_search
   end
 end
--- lib/social_tool.rb
+++ lib/social_tool.rb
@@ -1,13 +1,12 @@
 module SocialTool
   def self.twitter_search
-    client = Twitter::REST::Client.new do |config|
+    client = Tweetkit::Client.new do |config|
+      config.bearer_token        = ENV.fetch('TWITTER_BEARER_TOKEN_HERE')
       config.consumer_key        = ENV.fetch("TWITTER_CONSUMER_KEY")
       config.consumer_secret     = ENV.fetch("TWITTER_CONSUMER_SECRET")
-      config.access_token        = ENV.fetch("TWITTER_ACCESS_TOKEN")
-      config.access_token_secret = ENV.fetch("TWITTER_ACCESS_SECRET")
     end
-    client.search('#rubyonrails').take(3).each do |tweet|
-      client.update("@#{tweet.user} Hey I love Ruby too, what are your favorite blogs? :)")
+    client.search('#rubyonrails').take(6).collect do |tweet|
+      "#{tweet}: #{tweet.text}"
     end
   end
 end
