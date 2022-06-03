a5b67e32ad15eed7995fdc681d4a87f58f573634
Alex Vlasov <drumlife182@gmail.com>
2022-06-03 13:15:31 +0200

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.allowed_request_origins = ['https://alexvlasov-portfolio.herokuapp.com/', 'http://alexvlasov-portfolio.herokuapp.com/']
+  config.action_cable.url = 'wss://alexvlasov-portfolio.herokuapp.com/cable'
 end
